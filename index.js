f6cc90da9f2e4b293ab168fd7b8ffd571613b8d9
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 19:55:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelectorAll(selector)[0];
+}
+
+function nestedTarget(){
+  return document.getElementById("nested").querySelectorAll(".target")[0];
+}
+
+function increaseRankBy(n){
+  const lis=document.getElementsByClassName("ranked-list");
+  
+  for(let i=0; i<lis.length; i++){
+    lis[i].innerHTML=(parseInt(lis[i].innerHTML)+n);
+  }
+}
+
+function deepestChild(){
+  const node= document.getElementById("grand-node");
+  const divs=node.querySelectorAll("div");
+  return divs[divs.length-1];
+  
+  
+  
+}
