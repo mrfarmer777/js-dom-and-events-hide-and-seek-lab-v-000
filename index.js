b568cedbdd73c1f34fdfc8625b9b5a03d58c0d13
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 19:33:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.querySelectorAll("#nested.target")[0];
+  return document.querySelector("#nested.target")[0];
 }
