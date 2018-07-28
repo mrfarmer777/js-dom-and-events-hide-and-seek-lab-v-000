3d8d3f00b2b9812d2001efc17b0d38b8af685297
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 19:54:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function increaseRankBy(n){
 function deepestChild(){
   const node= document.getElementById("grand-node");
   const div=node.querySelectorAll("div")[3];
-  return div
+  return div;
   
   
   
